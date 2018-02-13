e79a707adb232b04d840a1b6ff7dec5127ae3275
Andy Maskiell <maskiella@gmail.com>
2018-02-13 01:00:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ module FormsLab
     end
 
     get '/new' do
-      erb :new
+      erb :pirates/new
     end
 
     post '/pirates' do
@@ -22,6 +22,6 @@ module FormsLab
 
       erb :show
     end
-  
+
   end
 end
