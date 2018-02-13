6b1b08f6ceb25831a63753b0cc9638a21a695019
Andy Maskiell <maskiella@gmail.com>
2018-02-13 01:31:36 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  ALL_PIRATEZ = []
+
+  def initialize(pirate_hash)
+    @name = pirate_hash[:name]
+    @weight = pirate_hash[:weight]
+    @height = pirate_hash[:height]
+    ALL_PIRATEZ << self
+  end
+
+  def self.all
+    ALL_PIRATEZ
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  ALL_SHIPZ = []
+
+  def initialize(ship_hash)
+    @name = ship_hash[:name]
+    @type = ship_hash[:type]
+    @booty = ship_hash[:booty]
+    ALL_SHIPZ << self
+  end
+
+  def self.all
+    ALL_SHIPZ
+  end
+
+  def self.clear
+    self.all.clear
+  end
+
+
 end
