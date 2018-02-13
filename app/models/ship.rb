4b5552bdd9d665b93157c82d9b4ecbb8e32f6efa
Andy Maskiell <maskiella@gmail.com>
2018-02-13 00:35:12 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  ALL_SHIPZ = []
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    ALL_SHIPZ << self
+  end
+
+  def self.all
+    ALL_SHIPZ
+  end
+  
 end
