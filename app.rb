a0ce14e6e2b9bb92ab323844c83608da95957db5
Andy Maskiell <maskiella@gmail.com>
2018-02-13 00:48:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
+      params[:pirate][:ships].each do |specs|
+        Ship.new(spec)
       end
     end
 
