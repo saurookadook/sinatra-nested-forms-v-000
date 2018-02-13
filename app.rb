57520515a471a59b0aeaecafad830a2037c47fca
Andy Maskiell <maskiella@gmail.com>
2018-02-13 00:46:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,10 +3,12 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
     post '/pirates' do
-      
+
     end
 
   end
